0afd924b2b51ea15c654a8ea2fd4824a3cedd5a1
Olleman82 <olle.soderqvist@gmail.com>
2026-02-09 20:41:39 +0100

    chore: remove internal tools and scripts from git tracking

--- WsprPc/find_select.ps1
+++
@@ -1,7 +0,0 @@
-$path = 'd:\Appar\wspr-pc\WsprPc\MainWindow.xaml.cs'
-$lines = [System.IO.File]::ReadAllLines($path)
-for ($i = 0; $i -lt $lines.Count; $i++) {
-    if ($lines[$i] -like '*SelectAudioFile*') {
-        Write-Output "$($i + 1): $($lines[$i])"
-    }
-}
